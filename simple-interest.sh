d4b580d5693c8ceda943258e048ae989ffe26d98
Tran Tan Phat <92932583+iuh20015561@users.noreply.github.com>
2024-01-18 15:57:13 +0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,10 +2,10 @@
  # This script calculates simple interest given principal,
  # annual rate of interest and time period in years.
  # Do not use this in production. Sample purpose only.
- # ID: <your student id>
- # Author: <your name>
+ # ID: 20015561
+ # Author: Tran Tan Phat
  # Additional Authors: MR. NAM
- # <your GitHub username>
+ # iuh20015561
  # Input:
  # p, principal amount
  # t, time period in years
